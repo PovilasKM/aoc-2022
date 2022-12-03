07cac81d52a2b58143aac430075cf86a2c1e708a
Kazimieras Povilas Maceika (fxl115) <kazimieras-povilas.maceika@teliacompany.com>
2022-12-03 12:27:17 +0200

    day 3 task

--- day3/main1.py
+++ day3/main1.py
@@ -1,4 +1,9 @@
-
+total_sum = 0
 for line in open('data.txt', "r"):
-    line
-print("helllo")
+    left, right = line[:len(line) // 2], line[len(line) // 2:]
+    for letter in left:
+        if letter in right:
+            total_sum += ord(letter) - 96 if letter.islower() else ord(letter) - 38
+            break
+
+print(total_sum)
--- day3/main2.py
+++ day3/main2.py
@@ -1,4 +1,10 @@
+total_sum = 0
+f = open('data.txt', "r")
+while True:
+    line1, line2, line3 = f.readline(), f.readline(), f.readline()
+    if not line1:
+        break
+    letter = (set(line1[:-1:]) & set(line2[:-1:]) & set(line3[:-1:])).pop()
+    total_sum += ord(letter) - 96 if letter.islower() else ord(letter) - 38
 
-for line in open('data.txt', "r"):
-    line
-print("helllo")
+print(total_sum)
