db2cc89df3b9da63a370dc4c19cf54b15d90b1da
Kazimieras Povilas Maceika (fxl115) <kazimieras-povilas.maceika@teliacompany.com>
2022-12-04 13:29:25 +0200

    day 4

--- day4/main2.py
+++ day4/main2.py
@@ -1,4 +1,10 @@
-
-for line in open('data.txt', "r"):
-    stuff = 0
-
+total = 0
+for linen in open('data.txt', "r"):
+    line = linen.rstrip("\n")
+    first_group = line.split(",")[0].split("-")
+    second_group = line.split(",")[1].split("-")
+    first_area = set(range(int(first_group[0]), int(first_group[1])+1))
+    second_area = set(range(int(second_group[0]), int(second_group[1])+1))
+    if len(first_area & second_area):
+        total += 1
+print(total)
---
+++ day5/main1.py
@@ -0,0 +1,4 @@
+total = 0
+for linen in open('data.txt', "r"):
+    line = linen.rstrip("\n")
+print(total)
