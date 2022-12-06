3927ad01f0e9ac671949a55a72ea6f06853c2159
Kazimieras Povilas Maceika (fxl115) <kazimieras-povilas.maceika@teliacompany.com>
2022-12-06 09:33:33 +0200

    day 6

--- day6/main1.py
+++ day6/main1.py
@@ -1,4 +1,2 @@
-total = 0
-for linen in open('data.txt', "r"):
-    line = linen.strip()
-print(total)
+line = open('data.txt', "r").readline().strip()
+print([i for i in range(3, len(line)) if len(set(line[i - 4:i])) == 4][0])
--- day6/main2.py
+++ day6/main2.py
@@ -1,4 +1,2 @@
-total = 0
-for linen in open('data.txt', "r"):
-    line = linen.strip()
-print(total)
+line = open('data.txt', "r").readline().strip()
+print([i for i in range(13, len(line)) if len(set(line[i - 14:i])) == 14][0])
