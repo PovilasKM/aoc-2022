ce3a110522fbfdedfa77f32225c7a325aa9efde4
Kazimieras Povilas Maceika (fxl115) <kazimieras-povilas.maceika@teliacompany.com>
2022-12-08 11:40:13 +0200

    day 8

--- day8/main1.py
+++ day8/main1.py
@@ -1,1 +1,13 @@
-# wololo
+forest = [(list(x)) for x in open('data.txt', "r").read().split("\n")]
+forest_visibility = [row[:] for row in forest]
+length = len(forest)
+for x in range(length):
+    for y in range(length):
+        cur = forest[x][y]
+        forest_visibility[x][y] = 0
+        if all(v[y] < cur for v in forest[:x]) \
+            or all(v[y] < cur for v in forest[x + 1:]) \
+            or all(v < cur for v in forest[x][:y]) \
+            or all(v < cur for v in forest[x][y + 1:]):
+            forest_visibility[x][y] = 1
+print(sum(x for x in sum(forest_visibility, []) if x == 1))
--- day8/main2.py
+++ day8/main2.py
@@ -1,1 +1,17 @@
-# wololo
+from itertools import takewhile
+
+forest = [(list(x)) for x in open('data.txt', "r").read().split("\n")]
+forest_scores = [row[:] for row in forest]
+length = len(forest)
+for x in range(1, length - 1):
+    for y in range(1, length - 1):
+        cur = forest[x][y]
+        distances = [len(list(takewhile(lambda v: v[y] < cur, forest[x - 1::-1]))),
+                     len(list(takewhile(lambda v: v[y] < cur, forest[x + 1:]))) + 1,
+                     len(list(takewhile(lambda v: v < cur, forest[x][y - 1::-1]))),
+                     len(list(takewhile(lambda v: v < cur, forest[x][y + 1:])))]
+        score = 1
+        for disc in distances:
+            score *= disc
+        forest_scores[x][y] = score
+print(max(sum([y[1:length - 1] for y in forest_scores[1:length - 1]], [])))
