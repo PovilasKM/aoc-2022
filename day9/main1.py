40b338732052ee61ae2de1708ce82885fe454b28
Kazimieras Povilas Maceika (fxl115) <kazimieras-povilas.maceika@teliacompany.com>
2022-12-09 11:16:46 +0200

    day 9 task. to be improved

--- day9/main1.py
+++ day9/main1.py
@@ -1,1 +1,20 @@
-# wololo
+# (x, y)
+directions = {"R": (1, 0), "U": (0, 1), "L": (-1, 0), "D": (0, -1)}
+f = open('data.txt', "r")
+visited = set()
+visited.add((0, 0))
+hcord = (0, 0)
+tcord = (0, 0)
+line = f.readline().strip()
+while line:
+    direction, distance = line.split(" ")
+    for _ in range(int(distance)):
+        hcord = (hcord[0] + directions[direction][0], hcord[1] + directions[direction][1])
+        xdist, ydist = hcord[0] - tcord[0], hcord[1] - tcord[1]
+        if 1 >= xdist >= -1 and 1 >= ydist >= -1:
+            continue  # don't move t
+        tcord = (tcord[0] + (1 if xdist >= 1 else -1 if xdist <= -1 else 0),
+                 tcord[1] + (1 if ydist >= 1 else -1 if ydist <= -1 else 0))
+        visited.add(tcord)
+    line = f.readline().strip()
+print("unique: ", len(visited))
--- day9/main2.py
+++ day9/main2.py
@@ -1,1 +1,25 @@
-# wololo
+# (x, y)
+directions = {"R": (1, 0), "U": (0, 1), "L": (-1, 0), "D": (0, -1)}
+f = open('data.txt', "r")
+visited = set()
+visited.add((0, 0))
+hcord = (0, 0)
+tcords = [(0, 0) for x in range(9)]
+line = f.readline().strip()
+while line:
+    direction, distance = line.split(" ")
+    for _ in range(int(distance)):
+        hcord = (hcord[0] + directions[direction][0], hcord[1] + directions[direction][1])
+        current_hcord = hcord
+        for idx, current_tcord in enumerate(tcords):
+            xdist, ydist = current_hcord[0] - current_tcord[0], current_hcord[1] - current_tcord[1]
+            if 1 >= xdist >= -1 and 1 >= ydist >= -1:
+                current_hcord = current_tcord
+                continue  # don't move t
+            tcords[idx] = (current_tcord[0] + (1 if xdist >= 1 else -1 if xdist <= -1 else 0),
+                           current_tcord[1] + (1 if ydist >= 1 else -1 if ydist <= -1 else 0))
+            if idx == 8:
+                visited.add(current_tcord)
+            current_hcord = tcords[idx]
+    line = f.readline().strip()
+print("unique: ", len(visited) + 1)  # no idea why but it works with +1
