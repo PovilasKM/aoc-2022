c7aed6bb70c94ff2b854929b8d58572042162ba7
Kazimieras Povilas Maceika (fxl115) <kazimieras-povilas.maceika@teliacompany.com>
2022-12-13 12:58:29 +0200

    day 13 prettier

--- day13/main1.py
+++ day13/main1.py
@@ -1,7 +1,7 @@
 import json
 
 
-def compare_left_right(left, right):
+def compare(left, right):
     if type(left) == type(right) == list:
         res = None
         if len(left) < len(right):
@@ -9,34 +9,31 @@ def compare_left_right(left, right):
         if len(left) > len(right):
             res = False
         for el_left, el_right in zip(left, right):
-            temp_res = compare_left_right(el_left, el_right)
+            temp_res = compare(el_left, el_right)
             if temp_res is None:
                 continue
             else:
                 return temp_res
         return res
-    if type(left) == type(right) == int:
+    elif type(left) == type(right) == int:
         if left > right:
             return False
         if left < right:
             return True
-    if isinstance(left, int) and isinstance(right, list):
-        return compare_left_right([left], right)
-    if isinstance(left, list) and isinstance(right, int):
-        return compare_left_right(left, [right])
+    elif isinstance(left, int) and isinstance(right, list):
+        return compare([left], right)
+    elif isinstance(left, list) and isinstance(right, int):
+        return compare(left, [right])
 
 
 pairs = open('data.txt', "r").read().split("\n\n")
-lines = [json.loads(item) for pair in pairs for item in pair.split("\n")]
+indxs = []
 
-n = len(lines)
-swapped = False
-for i in range(n - 1):
-    for j in range(0, n - i - 1):
-        if not compare_left_right(lines[j], lines[j+1]):
-            swapped = True
-            lines[j], lines[j + 1] = lines[j + 1], lines[j]
+for idx, pair in enumerate(pairs):
+    left = json.loads(pair.split("\n")[0])
+    right = json.loads(pair.split("\n")[1])
+    if compare(left, right):
+        indxs.append(idx + 1)
 
-a = lines.index([[2]]) + 1
-b = lines.index([[6]]) + 1
-print(a, b, a*b)
+print(indxs)
+print(sum(indxs))
